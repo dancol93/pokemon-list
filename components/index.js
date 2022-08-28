4fb6568844dd2352fb3d23d908b0ce3987ead7ce
Daniel Colas <danielenrique93@gmail.com>
2022-08-28 17:19:37 +0200

    chore: export components from index.js

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import PokemonBreadcrumbs from './PokemonBreadcrumbs/PokemonBreadcrumbs'
+import PokemonDetails from './PokemonDetails/PokemonDetails'
+import PokemonMoves from './PokemonMoves/PokemonMoves'
+
+export {
+  PokemonBreadcrumbs,
+  PokemonDetails,
+  PokemonMoves,
+}
